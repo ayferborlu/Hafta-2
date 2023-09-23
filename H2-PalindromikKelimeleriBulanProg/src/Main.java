fac14b5776e8b13391c85733435d9fd2289bfca6
ayferborlu <142981819+ayferborlu@users.noreply.github.com>
2023-09-23 21:59:03 +0300

    Palindromik kelimeleri bulan program

---
+++ H2-PalindromikKelimeleriBulanProg/src/Main.java
@@ -0,0 +1,23 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+
+    static boolean isPalindrom(String str) {
+        int i = 0;
+        int j = str.length() - 1;
+        while (i < j) {
+            if (str.charAt(i) != str.charAt(j)) {
+
+            return false;
+        }
+        i++;
+        j--;
+    }
+    return true;
+}
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrom("kavak"));
+
+    }
+}
